e4ad2dc2efb9e05c7376a5f5ba94de18c38b03bc
Gabriela Santos <gabi-assantos@hotmail.com>
2018-11-26 22:17:01 -0200

    mais um commit

---
+++ 1-basics/1-variables.py
@@ -0,0 +1,17 @@
+# text - variável de texto
+name = 'Gabriela' 
+
+# numbers - variável numérica
+age = 22
+height = 1.66
+
+# logical - verdeiro ou falso
+have_python_skills = True
+like_football = False
+
+# print - exibe no console
+print('name: ' + name)
+print('age: ' + str(age))
+print('height:', height)
+print('have_python_skills: ' + str(have_python_skills))
+print('like_football:', like_football)
